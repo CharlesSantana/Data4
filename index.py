4b1a7dad40e72c3b815f46f3f39d03ebd6f73613
Charles Santana <61568309+CharlesSantana@users.noreply.github.com>
2024-06-11 12:21:09 -0300

    Update index.py

--- index.py
+++ index.py
@@ -6,7 +6,7 @@
 
 import pandas as pd
 import streamlit as st
-import matplotlib.pyplot as plt
+#import matplotlib.pyplot as plt
 import numpy as np
 import plotly.express as px
 import plotly.graph_objects as go
